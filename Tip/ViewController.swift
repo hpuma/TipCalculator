b7fc3802a4cf7acf35066603dc3d65f5eeb96a0b
hpuma <pumahenry@live.com>
2020-11-28 21:34:11 -0500

    Updated README to include gif

--- Tip/ViewController.swift
+++ Tip/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        billAmountTextField.keyboardType = UIKeyboardType.default
         // Do any additional setup after loading the view.
     }
     
